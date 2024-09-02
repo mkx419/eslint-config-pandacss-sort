29a5c59d37f313d85f50061c538058598ded5777
mkx419 <mkx419@gmail.com>
2024-09-02 21:45:13 +0900

    refactor: use eslint-plugin-perfectionist's type

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,15 @@
+import type { rules } from "eslint-plugin-perfectionist";
+
 import { loadConfig } from "@pandacss/config";
 import path from "path";
 
-export default async function getConfig({
-  destructureOnly = false,
-  ignoreCase = true,
-  ignorePattern = [],
-  order = "asc",
-  partitionByComment = false,
-  partitionByNewLine = false,
-  pluginName = "perfectionist",
-  styledComponents = true,
-  type = "alphabetical",
-}: {
-  destructureOnly?: boolean;
-  ignoreCase?: boolean;
-  ignorePattern?: string[];
-  order?: "asc" | "desc";
-  partitionByComment?: boolean | string | string[];
-  partitionByNewLine?: boolean;
-  pluginName?: string;
-  styledComponents?: boolean;
-  type?: "alphabetical" | "line-length" | "natural";
-}) {
+type Options = { pluginName?: string } & Omit<
+  (typeof rules)["sort-objects"]["defaultOptions"][0],
+  "customGroups" | "groups"
+>;
+
+export default async function getConfig(options?: Options) {
+  const { pluginName = "perfectionist", ...restOptions } = options;
   const config = (await loadConfig({ cwd: path.resolve() })).config;
   const utilities = [...Object.keys(config.utilities), "textStyle", "layerStyle"];
   const conditions = Object.keys(config.conditions);
@@ -31,6 +19,7 @@ export default async function getConfig({
       [`${pluginName}/sort-objects`]: [
         "error",
         {
+          ...restOptions,
           customGroups: {
             ...utilities.reduce(
               (previous, current) => Object.assign(previous, { [`pandacss/${current}`]: current }),
@@ -42,18 +31,10 @@ export default async function getConfig({
               {},
             ),
           },
-          destructureOnly,
           groups: [
             ...utilities.map((value) => `pandacss/${value}`),
             ...conditions.map((value) => `pandacss/_${value}`),
           ],
-          ignoreCase,
-          ignorePattern,
-          order,
-          partitionByComment,
-          partitionByNewLine,
-          styledComponents,
-          type,
         },
       ],
     },
